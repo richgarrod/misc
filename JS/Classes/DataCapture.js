b864082061a456701f9a12870ab7f927f5a8163d
richgarrod <=>
2016-09-03 15:10:02 +0100

    formatting

--- JS/Classes/DataCapture.js
+++ JS/Classes/DataCapture.js
@@ -1,126 +1,121 @@
 var classes = classes || {};
 
-(function(win, classes){
-	'use strict';
+(function(win, classes) {
+    'use strict';
 
-	/**
-	 * This class sets up data capture on the page
-	 *
-	 * @constructs DataCapture
-	 */
-	function DataCapture(reporter) {
-		this.reporter = reporter;
-	};
+    /**
+     * This class sets up data capture on the page
+     *
+     * @constructs DataCapture
+     */
+    function DataCapture(reporter) {
+        this.reporter = reporter;
+    };
 
-	/**
-	 * This function initialises the whole process of capturing data and is the public interface
-	 * 
-	 * @name init
-	 */
-	DataCapture.prototype.init = function(mappings) {
-		mappings.forEach(function(mapping) {
-			if(mapping.event === 'onLoad') {
-				this.sendOnLoadEvent(mapping);
-			}
-			else {
-				this.setOnChangeListener(mapping);
-			}
-		}, this);
-	};
+    /**
+     * This function initialises the whole process of capturing data and is the public interface
+     * 
+     * @name init
+     */
+    DataCapture.prototype.init = function(mappings) {
+        mappings.forEach(function(mapping) {
+            if (mapping.event === 'onLoad') {
+                this.sendOnLoadEvent(mapping);
+            } else {
+                this.setOnChangeListener(mapping);
+            }
+        }, this);
+    };
 
-	/**
-	 * This function handles the on load events and sends their data via data reporter
-	 * 
-	 * @name sendOnLoadEvent
-	 * @param {object} mapping - the mapping object defining the data to send
-	 */
-	DataCapture.prototype.sendOnLoadEvent = function(mapping) {
-		// As they're classes there can be multiple elements, so we use querySelectorAll
-		var elements = document.querySelectorAll(mapping.selector);
-		
-		elements.forEach(function (element) {
-			var data = this.getValueFromElement(mapping, element);
-			// We don't want to send null data, this indicates an invalid entry ('' is not invalid)
-			if(data !== null) {
-				this.reporter.send(mapping.id, data)
-			};
-		}, this);
-	};
+    /**
+     * This function handles the on load events and sends their data via data reporter
+     * 
+     * @name sendOnLoadEvent
+     * @param {object} mapping - the mapping object defining the data to send
+     */
+    DataCapture.prototype.sendOnLoadEvent = function(mapping) {
+        // As they're classes there can be multiple elements, so we use querySelectorAll
+        var elements = document.querySelectorAll(mapping.selector);
 
-	/**
-	 * This function sets up the on change listeners for the different elements
-	 * 
-	 * @name setOnChangeListener
-	 * @param {object} mapping - the mapping object defining the data to send
-	 */
-	DataCapture.prototype.setOnChangeListener = function (mapping) {
-		// Whilst they're mostly ID selectors, there is one input selector and they MIGHT not be ids, they just happen to be
-		var elements = document.querySelectorAll(mapping.selector);
-		
-		// var reporter = this.reporter;
+        elements.forEach(function(element) {
+            var data = this.getValueFromElement(mapping, element);
+            // We don't want to send null data, this indicates an invalid entry ('' is not invalid)
+            if (data !== null) {
+                this.reporter.send(mapping.id, data)
+            };
+        }, this);
+    };
 
-		elements.forEach(function (element) {
-			element.addEventListener('change', function () {
-				var data = this.getValueFromElement(mapping, element);
-				// Again null indicates invalid data
-				if(data !== null) {
-					this.reporter.send(mapping.id, data);
-				}
-			}.bind(this))
-		}, this)
-	};
-	
-	/**
-	 * Simple utility function for getting desired data from the DOM element
-	 * 
-	 * @name getValueFromElement
-	 * @param {object} mapping - the mapping object defining the data to send
-	 * @param {object} element - the dom element we are reading the data from
-	 */
-	DataCapture.prototype.getValueFromElement = function (mapping, element) {
-		var value;
-		
-		if(mapping.attribute === 'text') {
-			value = element.innerHTML;
-		}
-		else if(mapping.attribute === 'value') {
-			value = element.value;
-		}
-		else if(mapping.attribute === 'checkbox') {
-			value = element.checked ? 'Checked' : 'Unchecked';
-		}
-		else if(mapping.attribute === 'radio') {
-			value = element.checked ? element.value : '';
-		}
-		else {
-			throw new Error("Unexpected attribute: " + mapping.attribute);
-		}
+    /**
+     * This function sets up the on change listeners for the different elements
+     * 
+     * @name setOnChangeListener
+     * @param {object} mapping - the mapping object defining the data to send
+     */
+    DataCapture.prototype.setOnChangeListener = function(mapping) {
+        // Whilst they're mostly ID selectors, there is one input selector and they MIGHT not be ids, they just happen to be
+        var elements = document.querySelectorAll(mapping.selector);
 
-		return this.validateValue(mapping, value);
-	};
+        // var reporter = this.reporter;
 
-	/**
-	 * Simple utility function for ensuring the value in the element is valid
-	 * 
-	 * @name validateValue
-	 * @param {object} mapping - the mapping object defining the data to send
-	 * @param {string} value - the value to be checked
-	 */
-	DataCapture.prototype.validateValue = function (mapping, value) {
-		if(mapping.isPhoneNumber) {
-			// Regex taken from http://www.regexlib.com/UserPatterns.aspx?authorid=d95177b0-6014-4e73-a959-73f1663ae814&AspxAutoDetectCookieSupport=1
-			var phoneNumberRegex = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/;
-			return phoneNumberRegex.test(value) ? value : null;
-		}
-		if(mapping.isEmail) {
-			// Regex taken from http://emailregex.com/
-			var emailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-			return emailRegex.test(value) ? value : null;
+        elements.forEach(function(element) {
+            element.addEventListener('change', function() {
+                var data = this.getValueFromElement(mapping, element);
+                // Again null indicates invalid data
+                if (data !== null) {
+                    this.reporter.send(mapping.id, data);
+                }
+            }.bind(this))
+        }, this)
+    };
 
-		}
-		return value;
-	};
+    /**
+     * Simple utility function for getting desired data from the DOM element
+     * 
+     * @name getValueFromElement
+     * @param {object} mapping - the mapping object defining the data to send
+     * @param {object} element - the dom element we are reading the data from
+     */
+    DataCapture.prototype.getValueFromElement = function(mapping, element) {
+        var value;
 
-	classes.DataCapture = DataCapture;
+        if (mapping.attribute === 'text') {
+            value = element.innerHTML;
+        } else if (mapping.attribute === 'value') {
+            value = element.value;
+        } else if (mapping.attribute === 'checkbox') {
+            value = element.checked ? 'Checked' : 'Unchecked';
+        } else if (mapping.attribute === 'radio') {
+            value = element.checked ? element.value : '';
+        } else {
+            throw new Error("Unexpected attribute: " + mapping.attribute);
+        }
+
+        return this.validateValue(mapping, value);
+    };
+
+    /**
+     * Simple utility function for ensuring the value in the element is valid
+     * 
+     * @name validateValue
+     * @param {object} mapping - the mapping object defining the data to send
+     * @param {string} value - the value to be checked
+     */
+    DataCapture.prototype.validateValue = function(mapping, value) {
+        if (mapping.isPhoneNumber) {
+            // Regex taken from http://www.regexlib.com/UserPatterns.aspx?authorid=d95177b0-6014-4e73-a959-73f1663ae814&AspxAutoDetectCookieSupport=1
+            var phoneNumberRegex = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/;
+            return phoneNumberRegex.test(value) ? value : null;
+        }
+        if (mapping.isEmail) {
+            // Regex taken from http://emailregex.com/
+            var emailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+            return emailRegex.test(value) ? value : null;
+
+        }
+        return value;
+    };
+
+    classes.DataCapture = DataCapture;
 
 }(window, classes));
--- Test/Classes/DataReporterTest.js
+++ Test/Classes/DataReporterTest.js
@@ -1,138 +1,138 @@
-(function(win, classes){
-  'use strict';
+(function(win, classes) {
+    'use strict';
 
-  describe('DataReporter', function () {
-    var DataReporter = classes.DataReporter,
-      dataReporterInstance;
+    describe('DataReporter', function() {
+        var DataReporter = classes.DataReporter,
+            dataReporterInstance;
 
-    describe('constructor', function () {
-      beforeEach(function () {
-        dataReporterInstance = new DataReporter();
-      });
+        describe('constructor', function() {
+            beforeEach(function() {
+                dataReporterInstance = new DataReporter();
+            });
 
-      it('initalizes the stored data object as empty', function () {
-        expect(dataReporterInstance.data).toEqual({});
-      });
+            it('initalizes the stored data object as empty', function() {
+                expect(dataReporterInstance.data).toEqual({});
+            });
+        });
+
+        describe('store', function() {
+            var hasBeenStored;
+
+            beforeEach(function() {
+                dataReporterInstance = new DataReporter();
+            });
+
+            describe('the mapping has never been stored', function() {
+                beforeEach(function() {
+                    dataReporterInstance.data = {};
+                });
+
+                it('stores the data in the data object', function() {
+                    dataReporterInstance.store(1, 'data');
+
+                    expect(dataReporterInstance.data).toEqual(jasmine.objectContaining({
+                        1: 'data'
+                    }));
+                });
+                it('returns that the data has been stored successfully', function() {
+                    hasBeenStored = dataReporterInstance.store(1, 'data');
+
+                    expect(hasBeenStored).toEqual(true);
+                });
+                it('does not remove the data stored previously', function() {
+                    dataReporterInstance.data = {
+                        1: 'data'
+                    };
+
+                    hasBeenStored = dataReporterInstance.store(2, 'more-data');
+
+                    expect(dataReporterInstance.data).toEqual(jasmine.objectContaining({
+                        1: 'data'
+                    }));
+                });
+            });
+
+            describe('the id has already been stored so we change the data assigned to it', function() {
+                beforeEach(function() {
+                    dataReporterInstance.data = {
+                        1: 'data'
+                    };
+
+                });
+
+                it('changes the stored data', function() {
+                    dataReporterInstance.store(1, 'new-data');
+
+                    expect(dataReporterInstance.data).toEqual({
+                        1: 'new-data'
+                    });
+                });
+                it('returns that the data has been stored successfully', function() {
+                    hasBeenStored = dataReporterInstance.store(1, 'new-data');
+
+                    expect(hasBeenStored).toEqual(true);
+                });
+            });
+
+            describe('the id has already been stored and the data we try to store is the same', function() {
+                beforeEach(function() {
+                    dataReporterInstance.data = {
+                        1: 'data'
+                    };
+                });
+
+                it('does not change the stored data', function() {
+                    dataReporterInstance.store(1, 'data');
+
+                    expect(dataReporterInstance.data[1]).toEqual('data');
+                });
+                it('returns that the data was the same and has not been stored', function() {
+                    hasBeenStored = dataReporterInstance.store(1, 'data');
+
+                    expect(hasBeenStored).toEqual(false);
+                });
+            });
+        });
+
+        describe('makeRequest', function() {
+            beforeEach(function() {
+                dataReporterInstance = new DataReporter();
+                spyOn(win.console, 'log');
+            });
+
+            it('logs that the data has been sent to the server', function() {
+                dataReporterInstance.makeRequest(1, 'data');
+
+                expect(win.console.log).toHaveBeenCalledWith('dataCaptured: mapping id: 1 - data: data');
+            });
+        });
+
+        describe('send', function() {
+            beforeEach(function() {
+                dataReporterInstance = new DataReporter();
+                spyOn(dataReporterInstance, 'store');
+                spyOn(dataReporterInstance, 'makeRequest');
+            });
+
+            it('stores the captured value in memory', function() {
+                dataReporterInstance.send(1, 'data');
+
+                expect(dataReporterInstance.store).toHaveBeenCalledWith(1, 'data');
+            });
+            it('does the request to the server if the value has changed', function() {
+                dataReporterInstance.store.and.returnValue(true);
+
+                dataReporterInstance.send(1, 'data');
+
+                expect(dataReporterInstance.makeRequest).toHaveBeenCalledWith(1, 'data');
+            });
+            it('does not do the request to the server if the value has not changed', function() {
+                dataReporterInstance.store.and.returnValue(false);
+
+                dataReporterInstance.send(1, 'data');
+
+                expect(dataReporterInstance.makeRequest).not.toHaveBeenCalled();
+            });
+        });
     });
-
-    describe('store', function () {
-      var hasBeenStored;
-
-      beforeEach(function () {
-        dataReporterInstance = new DataReporter();
-      });
-
-      describe('the mapping has never been stored', function () {
-        beforeEach(function () {
-          dataReporterInstance.data = {};
-        });
-
-        it('stores the data in the data object', function () {
-          dataReporterInstance.store(1, 'data');
-
-          expect(dataReporterInstance.data).toEqual(jasmine.objectContaining({
-            1: 'data'
-          }));
-        });       
-        it('returns that the data has been stored successfully', function () {
-          hasBeenStored = dataReporterInstance.store(1, 'data');
-
-          expect(hasBeenStored).toEqual(true);
-        });
-        it('does not remove the data stored previously', function () {
-          dataReporterInstance.data = {
-            1: 'data'
-          };
-
-          hasBeenStored = dataReporterInstance.store(2, 'more-data');
-
-          expect(dataReporterInstance.data).toEqual(jasmine.objectContaining({
-            1: 'data'
-          }));
-        });
-      });
-
-      describe('the id has already been stored so we change the data assigned to it', function () {
-        beforeEach(function () {
-          dataReporterInstance.data = {
-            1: 'data'
-          };
-
-        });
-
-        it('changes the stored data', function () {
-          dataReporterInstance.store(1, 'new-data');
-
-          expect(dataReporterInstance.data).toEqual({
-            1: 'new-data'
-          });
-        });
-        it('returns that the data has been stored successfully', function () {
-          hasBeenStored = dataReporterInstance.store(1, 'new-data');
-          
-          expect(hasBeenStored).toEqual(true);
-        });
-      });
-
-      describe('the id has already been stored and the data we try to store is the same', function () {
-        beforeEach(function () {
-          dataReporterInstance.data = {
-            1: 'data'
-          };
-        });
-
-        it('does not change the stored data', function () {
-          dataReporterInstance.store(1, 'data');
-          
-          expect(dataReporterInstance.data[1]).toEqual('data');
-        });
-        it('returns that the data was the same and has not been stored', function () {
-          hasBeenStored = dataReporterInstance.store(1, 'data');
-
-          expect(hasBeenStored).toEqual(false);
-        });
-      });
-    });
-
-    describe('makeRequest', function () {
-      beforeEach(function () {
-        dataReporterInstance = new DataReporter();
-        spyOn(win.console, 'log');
-      });
-
-      it('logs that the data has been sent to the server', function () {
-        dataReporterInstance.makeRequest(1, 'data');
-
-        expect(win.console.log).toHaveBeenCalledWith('dataCaptured: mapping id: 1 - data: data');
-      });
-    });
-
-    describe('send', function () {
-      beforeEach(function () {
-        dataReporterInstance = new DataReporter();
-        spyOn(dataReporterInstance, 'store');
-        spyOn(dataReporterInstance, 'makeRequest');
-      });
-
-      it('stores the captured value in memory', function () {
-        dataReporterInstance.send(1, 'data');
-
-        expect(dataReporterInstance.store).toHaveBeenCalledWith(1, 'data');
-      });
-      it('does the request to the server if the value has changed', function () {
-        dataReporterInstance.store.and.returnValue(true);
-
-        dataReporterInstance.send(1, 'data');
-
-        expect(dataReporterInstance.makeRequest).toHaveBeenCalledWith(1, 'data');
-      });
-      it('does not do the request to the server if the value has not changed', function () {
-        dataReporterInstance.store.and.returnValue(false);
-        
-        dataReporterInstance.send(1, 'data');
-
-        expect(dataReporterInstance.makeRequest).not.toHaveBeenCalled();
-      });
-    });
-  });
 }(window, classes));
